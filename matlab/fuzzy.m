4b3abe25e6f3437f5afd7d61e9028e5517764a20
EvenRL <even.r.landro@gmail.com>
2024-02-13 16:01:50 +0100

    Update fuzzy.m

--- matlab/fuzzy.m
+++ matlab/fuzzy.m
@@ -1,27 +1,27 @@
 %% Oppsett
 fis = mamfis("Name", "LFR");
 
-fis = addInput(fis, [0, 160], "Name", "Avvik");
-fis = addInput(fis, [-140, 140], "Name", "DAvvik");
+fis = addInput(fis, [0, 160], "Name", "LinjePos");
+fis = addInput(fis, [-140, 140], "Name", "DLinjePos");
 
 fis = addOutput(fis, [-150, 150], "Name", "StyreRetning");
 
 %% Input medlemsfunksjoner
 
-fis = addMF(fis, "Avvik", "zmf", [20, 35], "Name", "SV");
-fis = addMF(fis, "Avvik", "trapmf", [20, 35, 50, 70], "Name", "LV");
-fis = addMF(fis, "Avvik", "trapmf", [50, 70, 90, 110], "Name", "INGEN");
-fis = addMF(fis, "Avvik", "trapmf", [90, 110, 125, 140], "Name", "LH");
-fis = addMF(fis, "Avvik", "smf", [125, 140], "Name", "SH");
+fis = addMF(fis, "LinjePos", "zmf", [20, 35], "Name", "SV");
+fis = addMF(fis, "LinjePos", "trapmf", [20, 35, 50, 70], "Name", "LV");
+fis = addMF(fis, "LinjePos", "trapmf", [50, 70, 90, 110], "Name", "INGEN");
+fis = addMF(fis, "LinjePos", "trapmf", [90, 110, 125, 140], "Name", "LH");
+fis = addMF(fis, "LinjePos", "smf", [125, 140], "Name", "SH");
 
 plotmf(fis, "input", 1);
-%figure();
+figure();
 
-fis = addMF(fis, "DAvvik", "zmf", [-120, -85], "Name", "SVS");
-fis = addMF(fis, "DAvvik", "trapmf", [-120, -85, -50, -25], "Name", "LVS");
-fis = addMF(fis, "DAvvik", "trapmf", [-50, -25, 25, 50], "Name", "INGEN");
-fis = addMF(fis, "DAvvik", "trapmf", [25, 50, 85, 120], "Name", "LHS");
-fis = addMF(fis, "DAvvik", "smf", [85, 120], "Name", "SHS");
+fis = addMF(fis, "DLinjePos", "zmf", [-120, -85], "Name", "SVS");
+fis = addMF(fis, "DLinjePos", "trapmf", [-120, -85, -50, -25], "Name", "LVS");
+fis = addMF(fis, "DLinjePos", "trapmf", [-50, -25, 25, 50], "Name", "INGEN");
+fis = addMF(fis, "DLinjePos", "trapmf", [25, 50, 85, 120], "Name", "LHS");
+fis = addMF(fis, "DLinjePos", "smf", [85, 120], "Name", "SHS");
 
 plotmf(fis, "input", 2);
 %figure();
@@ -40,35 +40,35 @@ plotmf(fis, "output", 1);
 %% Regler
 
 regler = [
-    "If Avvik is SV and DAvvik is INGEN then StyreRetning is SVS";
-    "If Avvik is LV and DAvvik is INGEN then StyreRetning is LVS";
-    "If Avvik is INGEN and DAvvik is INGEN then StyreRetning is INGEN";
-    "If Avvik is LH and DAvvik is INGEN then StyreRetning is LHS";
-    "If Avvik is SH and DAvvik is INGEN then StyreRetning is SHS";
+    "If LinjePos is SV and DLinjePos is INGEN then StyreRetning is SVS";
+    "If LinjePos is LV and DLinjePos is INGEN then StyreRetning is LVS";
+    "If LinjePos is INGEN and DLinjePos is INGEN then StyreRetning is INGEN";
+    "If LinjePos is LH and DLinjePos is INGEN then StyreRetning is LHS";
+    "If LinjePos is SH and DLinjePos is INGEN then StyreRetning is SHS";
     
-    "If Avvik is SV and DAvvik is LVS then StyreRetning is SVS";
-    "If Avvik is LV and DAvvik is LVS then StyreRetning is LVS";
-    "If Avvik is INGEN and DAvvik is LVS then StyreRetning is INGEN";
-    "If Avvik is LH and DAvvik is LVS then StyreRetning is LHS";
-    "If Avvik is SH and DAvvik is LVS then StyreRetning is SHS";
+    "If LinjePos is SV and DLinjePos is LVS then StyreRetning is SVS";
+    "If LinjePos is LV and DLinjePos is LVS then StyreRetning is LVS";
+    "If LinjePos is INGEN and DLinjePos is LVS then StyreRetning is INGEN";
+    "If LinjePos is LH and DLinjePos is LVS then StyreRetning is LHS";
+    "If LinjePos is SH and DLinjePos is LVS then StyreRetning is SHS";
     
-    "If Avvik is SV and DAvvik is SVS then StyreRetning is SVS";
-    "If Avvik is LV and DAvvik is SVS then StyreRetning is SVS";
-    "If Avvik is INGEN and DAvvik is SVS then StyreRetning is LVS";
-    "If Avvik is LH and DAvvik is SVS then StyreRetning is LVS";
-    "If Avvik is SH and DAvvik is SVS then StyreRetning is LVS";
+    "If LinjePos is SV and DLinjePos is SVS then StyreRetning is SVS";
+    "If LinjePos is LV and DLinjePos is SVS then StyreRetning is SVS";
+    "If LinjePos is INGEN and DLinjePos is SVS then StyreRetning is LVS";
+    "If LinjePos is LH and DLinjePos is SVS then StyreRetning is LVS";
+    "If LinjePos is SH and DLinjePos is SVS then StyreRetning is LVS";
     
-    "If Avvik is SV and DAvvik is LHS then StyreRetning is SVS";
-    "If Avvik is LV and DAvvik is LHS then StyreRetning is LVS";
-    "If Avvik is INGEN and DAvvik is LHS then StyreRetning is INGEN";
-    "If Avvik is LH and DAvvik is LHS then StyreRetning is LHS";
-    "If Avvik is SH and DAvvik is LHS then StyreRetning is SHS";
+    "If LinjePos is SV and DLinjePos is LHS then StyreRetning is SVS";
+    "If LinjePos is LV and DLinjePos is LHS then StyreRetning is LVS";
+    "If LinjePos is INGEN and DLinjePos is LHS then StyreRetning is INGEN";
+    "If LinjePos is LH and DLinjePos is LHS then StyreRetning is LHS";
+    "If LinjePos is SH and DLinjePos is LHS then StyreRetning is SHS";
     
-    "If Avvik is SV and DAvvik is SHS then StyreRetning is LHS";
-    "If Avvik is LV and DAvvik is SHS then StyreRetning is LHS";
-    "If Avvik is INGEN and DAvvik is SHS then StyreRetning is LHS";
-    "If Avvik is LH and DAvvik is SHS then StyreRetning is SHS";
-    "If Avvik is SH and DAvvik is SHS then StyreRetning is SHS";];
+    "If LinjePos is SV and DLinjePos is SHS then StyreRetning is LHS";
+    "If LinjePos is LV and DLinjePos is SHS then StyreRetning is LHS";
+    "If LinjePos is INGEN and DLinjePos is SHS then StyreRetning is LHS";
+    "If LinjePos is LH and DLinjePos is SHS then StyreRetning is SHS";
+    "If LinjePos is SH and DLinjePos is SHS then StyreRetning is SHS";];
 
 fis = addRule(fis, regler);
 
