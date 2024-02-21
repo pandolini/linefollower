40765089fa214a583ea73b711fac4109f1957d8b
EvenRL <even.r.landro@gmail.com>
2024-02-21 23:48:04 +0100

    Convert fuzzy to output PD parameters

--- matlab/fuzzy.m
+++ matlab/fuzzy.m
@@ -2,14 +2,15 @@
 fis = mamfis("Name", "LFR");
 
 fis = addInput(fis, [0, 140], "Name", "LinjePos");
-fis = addInput(fis, [-140, 140], "Name", "DLinjePos");
+fis = addInput(fis, [0, 140], "Name", "DLinjePos");
 
-fis = addOutput(fis, [-150, 150], "Name", "StyreRetning");
+fis = addOutput(fis, [0, 1], "Name", "Kp");
+fis = addOutput(fis, [0, 1], "Name", "Kd");
 
 %% Input medlemsfunksjoner
 
 fis = addMF(fis, "LinjePos", "zmf", [20, 35], "Name", "SV");
-fis = addMF(fis, "LinjePos", "trapmf", [20, 35, 50, 70], "Name", "LV");
+fis = addMF(fis, "LinjePos", "trapmf", [20, 35, 50, 60], "Name", "LV");
 fis = addMF(fis, "LinjePos", "trapmf", [50, 60, 80, 90], "Name", "MIDTEN");
 fis = addMF(fis, "LinjePos", "trapmf", [80, 90, 105, 120], "Name", "LH");
 fis = addMF(fis, "LinjePos", "smf", [105, 120], "Name", "SH");
@@ -17,73 +18,90 @@ fis = addMF(fis, "LinjePos", "smf", [105, 120], "Name", "SH");
 plotmf(fis, "input", 1);
 figure();
 
-fis = addMF(fis, "DLinjePos", "zmf", [-120, -85], "Name", "SVS");
-fis = addMF(fis, "DLinjePos", "trapmf", [-120, -85, -50, -25], "Name", "LVS");
-fis = addMF(fis, "DLinjePos", "trapmf", [-50, -25, 25, 50], "Name", "INGEN");
-fis = addMF(fis, "DLinjePos", "trapmf", [25, 50, 85, 120], "Name", "LHS");
-fis = addMF(fis, "DLinjePos", "smf", [85, 120], "Name", "SHS");
+fis = addMF(fis, "DLinjePos", "zmf", [20, 35], "Name", "INGEN");
+fis = addMF(fis, "DLinjePos", "trapmf", [20, 35, 50, 60], "Name", "XLS");
+fis = addMF(fis, "DLinjePos", "trapmf", [50, 60, 80, 90], "Name", "LS");
+fis = addMF(fis, "DLinjePos", "trapmf", [80, 90, 105, 120], "Name", "SS");
+fis = addMF(fis, "DLinjePos", "smf", [105, 120], "Name", "XSS");
 
 plotmf(fis, "input", 2);
 figure();
 
 %% Output medlemsfunksjoner
 
-fis = addMF(fis, "StyreRetning", "zmf", [-100, -80], "Name", "SVS");
-fis = addMF(fis, "StyreRetning", "trapmf", [-100, -80, -40, -20], "Name", "LVS");
-fis = addMF(fis, "StyreRetning", "trapmf", [-40, -20, 20, 40], "Name", "MIDTEN");
-fis = addMF(fis, "StyreRetning", "trapmf", [20, 40, 80, 100], "Name", "LHS");
-fis = addMF(fis, "StyreRetning", "smf", [80, 100], "Name", "SHS");
+fis = addMF(fis, "Kp", "zmf", [0, 0.2], "Name", "XL");
+fis = addMF(fis, "Kp", "trapmf", [0, 0.2, 0.3, 0.4], "Name", "L");
+fis = addMF(fis, "Kp", "trapmf", [0.3, 0.4, 0.6, 0.7], "Name", "M");
+fis = addMF(fis, "Kp", "trapmf", [0.6, 0.7, 0.8, 1], "Name", "S");
+fis = addMF(fis, "Kp", "smf", [0.8, 1], "Name", "XS");
 
 plotmf(fis, "output", 1);
-%figure();
+figure();
+
+fis = addMF(fis, "Kd", "zmf", [0, 0.2], "Name", "XL");
+fis = addMF(fis, "Kd", "trapmf", [0, 0.2, 0.3, 0.4], "Name", "L");
+fis = addMF(fis, "Kd", "trapmf", [0.3, 0.4, 0.6, 0.7], "Name", "M");
+fis = addMF(fis, "Kd", "trapmf", [0.6, 0.7, 0.8, 1], "Name", "S");
+fis = addMF(fis, "Kd", "smf", [0.8, 1], "Name", "XS");
+
+plotmf(fis, "output", 2);
+figure();
 
 %% Regler
 
 regler = [
-    "If LinjePos is SV and DLinjePos is INGEN then StyreRetning is SVS";
-    "If LinjePos is LV and DLinjePos is INGEN then StyreRetning is LVS";
-    "If LinjePos is MIDTEN and DLinjePos is INGEN then StyreRetning is MIDTEN";
-    "If LinjePos is LH and DLinjePos is INGEN then StyreRetning is LHS";
-    "If LinjePos is SH and DLinjePos is INGEN then StyreRetning is SHS";
+    "If LinjePos is SV and DLinjePos is INGEN then Kp is L, Kd is L";
+    "If LinjePos is LV and DLinjePos is INGEN then Kp is XL, Kd is XL";
+    "If LinjePos is MIDTEN and DLinjePos is INGEN then Kp is XL, Kd is XL";
+    "If LinjePos is LH and DLinjePos is INGEN then Kp is XL, Kd is XL";
+    "If LinjePos is SH and DLinjePos is INGEN then Kp is L, Kd is L";
     
-    "If LinjePos is SV and DLinjePos is LVS then StyreRetning is SVS";
-    "If LinjePos is LV and DLinjePos is LVS then StyreRetning is LVS";
-    "If LinjePos is MIDTEN and DLinjePos is LVS then StyreRetning is MIDTEN";
-    "If LinjePos is LH and DLinjePos is LVS then StyreRetning is LHS";
-    "If LinjePos is SH and DLinjePos is LVS then StyreRetning is SHS";
+    "If LinjePos is SV and DLinjePos is XLS then Kp is L, Kd is L";
+    "If LinjePos is LV and DLinjePos is XLS then Kp is L, Kd is L";
+    "If LinjePos is MIDTEN and DLinjePos is XLS then Kp is L, Kd is L";
+    "If LinjePos is LH and DLinjePos is XLS then Kp is L, Kd is L";
+    "If LinjePos is SH and DLinjePos is XLS then Kp is L, Kd is L";
     
-    "If LinjePos is SV and DLinjePos is SVS then StyreRetning is SVS";
-    "If LinjePos is LV and DLinjePos is SVS then StyreRetning is SVS";
-    "If LinjePos is MIDTEN and DLinjePos is SVS then StyreRetning is LVS";
-    "If LinjePos is LH and DLinjePos is SVS then StyreRetning is LVS";
-    "If LinjePos is SH and DLinjePos is SVS then StyreRetning is LVS";
+    "If LinjePos is SV and DLinjePos is LS then Kp is M, Kd is M";
+    "If LinjePos is LV and DLinjePos is LS then Kp is L, Kd is L";
+    "If LinjePos is MIDTEN and DLinjePos is LS then Kp is L, Kd is L";
+    "If LinjePos is LH and DLinjePos is LS then Kp is L, Kd is L";
+    "If LinjePos is SH and DLinjePos is LS then Kp is M, Kd is M";
     
-    "If LinjePos is SV and DLinjePos is LHS then StyreRetning is SVS";
-    "If LinjePos is LV and DLinjePos is LHS then StyreRetning is LVS";
-    "If LinjePos is MIDTEN and DLinjePos is LHS then StyreRetning is MIDTEN";
-    "If LinjePos is LH and DLinjePos is LHS then StyreRetning is LHS";
-    "If LinjePos is SH and DLinjePos is LHS then StyreRetning is SHS";
+    "If LinjePos is SV and DLinjePos is SS then Kp is S, Kd is S";
+    "If LinjePos is LV and DLinjePos is SS then Kp is M, Kd is M";
+    "If LinjePos is MIDTEN and DLinjePos is SS then Kp is L, Kd is L";
+    "If LinjePos is LH and DLinjePos is SS then Kp is M, Kd is M";
+    "If LinjePos is SH and DLinjePos is SS then Kp is S, Kd is S";
     
-    "If LinjePos is SV and DLinjePos is SHS then StyreRetning is LHS";
-    "If LinjePos is LV and DLinjePos is SHS then StyreRetning is LHS";
-    "If LinjePos is MIDTEN and DLinjePos is SHS then StyreRetning is LHS";
-    "If LinjePos is LH and DLinjePos is SHS then StyreRetning is SHS";
-    "If LinjePos is SH and DLinjePos is SHS then StyreRetning is SHS";];
+    "If LinjePos is SV and DLinjePos is XSS then Kp is XS, Kd is XS";
+    "If LinjePos is LV and DLinjePos is XSS then Kp is S, Kd is S";
+    "If LinjePos is MIDTEN and DLinjePos is XSS then Kp is M, Kd is M";
+    "If LinjePos is LH and DLinjePos is XSS then Kp is S, Kd is S";
+    "If LinjePos is SH and DLinjePos is XSS then Kp is XS, Kd is XS";];
 
 fis = addRule(fis, regler);
 
-gensurf(fis);
+opt1 = gensurfOptions('OutputIndex', 1);
+opt2 = gensurfOptions('OutputIndex', 2);
+gensurf(fis, opt1);
+figure();
+gensurf(fis, opt2);
 %% 
 
 Step = 1;
 E = 0:Step:140;
-CE = -140:Step:140;
+CE = 0:Step:140;
 N = length(E);
-LookUpTableData = zeros(N);
+LookUpTableDataKp = zeros(N);
+LookUpTableDataKd = zeros(N);
 for i=1:length(E)
    for j=1:length(CE)
-      LookUpTableData(i,j) = round(evalfis(fis,[E(i) CE(j)]));
+      output = round(evalfis(fis,[E(i) CE(j)]), 3);
+      LookUpTableDataKp(i,j) = output(1);
+      LookUpTableDataKd(i,j) = output(2);
    end
 end
 
-writematrix(LookUpTableData, 'table.csv');
+writematrix(LookUpTableDataKp, 'tableKp.csv');
+writematrix(LookUpTableDataKd, 'tableKd.csv');
