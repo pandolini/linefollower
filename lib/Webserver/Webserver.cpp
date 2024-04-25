5256f5b1095d9aac316a6de5924551392613d89a
Rune <143501123+pandolini@users.noreply.github.com>
2024-04-25 20:19:18 +0200

    update HTML to get current values

--- lib/Webserver/Webserver.cpp
+++ lib/Webserver/Webserver.cpp
@@ -12,44 +12,50 @@ void Webserver::connectWiFi() {
 }
 
 void Webserver::setupServer(Logic& logic, MotorController& motorController) {
-    server_.on("/form", HTTP_GET, [](AsyncWebServerRequest* request) {
+    server_.on("/form", HTTP_GET, [&logic, &motorController](AsyncWebServerRequest* request) {
         String html = R"(
-            <form id="paramsForm">
-                Kp: <input type="text" name="Kp"><br>
-                Kd: <input type="text" name="Kd"><br>
-                Ki: <input type="text" name="Ki"><br>
-                Base speed: <input type="text" name="baseSpeed"><br>
-                Max speed: <input type="text" name="maxSpeed"><br>
-                Min speed: <input type="text" name="minSpeed"><br>
-                <input type="submit" value="Submit">
-            </form>
-            <button id="startMotorsButton" style="background-color: green;">Start Motors</button>
-            <button id="stopMotorsButton" style="background-color: red;">Stop Motors</button>
-            <button id="resetErrorsButton" style="background-color: blue;">Reset Errors</button>
-            <script>
-                document.getElementById('paramsForm').addEventListener('submit', function(event) {
-                    event.preventDefault();
-                    var xhr = new XMLHttpRequest();
-                    xhr.open('GET', '/set_params?' + new URLSearchParams(new FormData(event.target)).toString(), true);
-                    xhr.send();
-                });
-                document.getElementById('startMotorsButton').addEventListener('click', function() {
-                    var xhr = new XMLHttpRequest();
-                    xhr.open('GET', '/start_motors', true);
-                    xhr.send();
-                });
-                document.getElementById('stopMotorsButton').addEventListener('click', function() {
-                    var xhr = new XMLHttpRequest();
-                    xhr.open('GET', '/stop_motors', true);
-                    xhr.send();
-                });
-                document.getElementById('resetErrorsButton').addEventListener('click', function() {
-                    var xhr = new XMLHttpRequest();
-                    xhr.open('GET', '/reset_errors', true);
-                    xhr.send();
-                });
-            </script>
-        )";
+        <form id="paramsForm">
+            Kp: <input type="text" name="Kp" value=")" +
+                      String(logic.getKp()) + R"("><br>
+            Kd: <input type="text" name="Kd" value=")" +
+                      String(logic.getKd()) + R"("><br>
+            Ki: <input type="text" name="Ki" value=")" +
+                      String(logic.getKi()) + R"("><br>
+            Base speed: <input type="text" name="baseSpeed" value=")" +
+                      String(motorController.getBaseSpeed()) + R"("><br>
+            Max speed: <input type="text" name="maxSpeed" value=")" +
+                      String(motorController.getMaxSpeed()) + R"("><br>
+            Min speed: <input type="text" name="minSpeed" value=")" +
+                      String(motorController.getMinSpeed()) + R"("><br>
+            <input type="submit" value="Submit">
+        </form>
+        <button id="startMotorsButton" style="background-color: green;">Start Motors</button>
+        <button id="stopMotorsButton" style="background-color: red;">Stop Motors</button>
+        <button id="resetErrorsButton" style="background-color: blue;">Reset Errors</button>
+        <script>
+            document.getElementById('paramsForm').addEventListener('submit', function(event) {
+                event.preventDefault();
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', '/set_params?' + new URLSearchParams(new FormData(event.target)).toString(), true);
+                xhr.send();
+            });
+            document.getElementById('startMotorsButton').addEventListener('click', function() {
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', '/start_motors', true);
+                xhr.send();
+            });
+            document.getElementById('stopMotorsButton').addEventListener('click', function() {
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', '/stop_motors', true);
+                xhr.send();
+            });
+            document.getElementById('resetErrorsButton').addEventListener('click', function() {
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', '/reset_errors', true);
+                xhr.send();
+            });
+        </script>
+    )";
         request->send(200, "text/html", html);
     });
 
@@ -73,6 +79,18 @@ void Webserver::setupServer(Logic& logic, MotorController& motorController) {
             motorController.setMinSpeed(request->getParam("minSpeed")->value().toFloat());
         }
         request->send(200, "text/plain", "Parameters set");
+    });
+
+    server_.on("/get_params", HTTP_GET, [&logic, &motorController](AsyncWebServerRequest* request) {
+        String json = "{";
+        json += "\"Kp\":" + String(logic.getKp()) + ",";
+        json += "\"Kd\":" + String(logic.getKd()) + ",";
+        json += "\"Ki\":" + String(logic.getKi()) + ",";
+        json += "\"baseSpeed\":" + String(motorController.getBaseSpeed()) + ",";
+        json += "\"maxSpeed\":" + String(motorController.getMaxSpeed()) + ",";
+        json += "\"minSpeed\":" + String(motorController.getMinSpeed());
+        json += "}";
+        request->send(200, "application/json", json);
     });
 
     server_.on("/stop_motors", HTTP_GET, [&motorController](AsyncWebServerRequest* request) {
